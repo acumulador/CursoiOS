e2676a396366c8d2054db6e4e4ade2a2179f0e13
acumulador <jcsalazarm@gmail.com>
2014-09-29 17:25:03 -0500

    Commit de taller de Aplicación
    Proyecto Creado

---
+++ Convertidor Dolares/Convertidor Dolares/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Convertidor Dolares
+//
+//  Created by Juan C Salazar on 29/09/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
