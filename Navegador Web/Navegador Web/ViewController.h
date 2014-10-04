3715327237a4e6f740b95e737c2045715704a98a
acumulador <jcsalazarm@gmail.com>
2014-10-03 19:47:42 -0500

    Listo el navegador con barra de botones

--- Navegador Web/Navegador Web/ViewController.h
+++ Navegador Web/Navegador Web/ViewController.h
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIWebViewDelegate>
 
+
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *animationActivity;
+@property (strong, nonatomic) IBOutlet UIWebView *navegate;
+- (IBAction)htmlButton:(id)sender;
+- (IBAction)jsButton:(id)sender;
+- (IBAction)urlButton:(id)sender;
+- (IBAction)pdfButton:(id)sender;
 
 @end
 
--- Navegador Web/Navegador Web/ViewController.m
+++ Navegador Web/Navegador Web/ViewController.m
@@ -16,12 +16,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _navegate.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)urlButton:(id)sender {
+    NSURL * url = [NSURL URLWithString:@"http://edusoftglobal.com/academia"];
+    NSURLRequest * request = [[NSURLRequest alloc] initWithURL:url];
+    [_navegate loadRequest:request];
+}
+
+- (IBAction)pdfButton:(id)sender {
+    NSString * rutaPdf = [[NSBundle mainBundle] pathForResource:@"comfenalco" ofType:@"pdf"];
+    
+    NSData * dataPdf = [[NSData alloc] initWithContentsOfFile:rutaPdf];
+    [_navegate loadData:dataPdf MIMEType:@"application/pdf" textEncodingName:nil baseURL:nil];
+    [_navegate setScalesPageToFit:YES];
+}
+
+- (IBAction)htmlButton:(id)sender {
+    NSString * htmlString = @"<h1>Hola Página</h1><p style='color:green;'>Este parrafo lo monte yo!!</p>";
+    [_navegate loadHTMLString:htmlString baseURL:nil];
+}
+
+- (IBAction)jsButton:(id)sender {
+    NSString * jsString = @"alert('Hola con JS');";
+    [_navegate stringByEvaluatingJavaScriptFromString:jsString];
+}
+
+-(void) webViewDidStartLoad:(UIWebView *)webView
+{
+    _animationActivity.hidden=NO;
+    [_animationActivity startAnimating];
+}
+
+-(void) webViewDidFinishLoad:(UIWebView *)webView
+{
+    _animationActivity.hidden = YES;
+    [_animationActivity stopAnimating];
 }
 
 @end
