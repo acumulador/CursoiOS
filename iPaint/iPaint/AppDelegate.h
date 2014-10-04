1aa23892d1a1d241b4ef7183dabcb0e60f21ee2a
acumulador <jcsalazarm@gmail.com>
2014-10-03 19:52:58 -0500

    Primer commit de iPaint con segmentos

---
+++ iPaint/iPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPaint
+//
+//  Created by Juan C Salazar on 3/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
