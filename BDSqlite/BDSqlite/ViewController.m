d092480bf391c7df94e87abfde2dca4416cfb980
acumulador <jcsalazarm@gmail.com>
2014-10-08 17:40:12 -0500

    Primer commit
    ViewControllers con elementos del formulario para guardar

---
+++ BDSqlite/BDSqlite/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BDSqlite
+//
+//  Created by Juan C Salazar on 8/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
