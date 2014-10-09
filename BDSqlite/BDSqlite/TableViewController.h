7f9851b024861bb5f80696c1aa3ee510a176d69a
acumulador <jcsalazarm@gmail.com>
2014-10-09 01:41:24 -0500

    Ultimo commit con tabla
    No esta terminado lo de la tabla

---
+++ BDSqlite/BDSqlite/TableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TableViewController.h
+//  BDSqlite
+//
+//  Created by Juan C Salazar on 9/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UITableView *datosTableView;
+
+@end
