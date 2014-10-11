a2ae16bd87d2c32d4040c7e2e2df0e7f8bee144c
acumulador <jcsalazarm@gmail.com>
2014-10-10 20:30:08 -0500

    Primer commit
    Proyecto de Mercado

---
+++ Lista de Mercado/Lista de Mercado/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lista de Mercado
+//
+//  Created by Juan C Salazar on 10/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
