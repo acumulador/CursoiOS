7a9a82c725fa6692a309b9090b805f04fe80e6d4
Jhon Orrego <jhonwi60@gmail.com>
2014-10-12 19:35:07 -0500

    tableviews

---
+++ Lista de Mercado/Lista de Mercado/CategoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoryTableViewController.h
+//  Lista de Mercado
+//
+//  Created by Jhon Wilfer Orrego on 12/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryTableViewController : UITableViewController
+
+@end
---
+++ Lista de Mercado/Lista de Mercado/ListMarketTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListMarketTableViewController.h
+//  Lista de Mercado
+//
+//  Created by Jhon Wilfer Orrego on 12/10/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListMarketTableViewController : UITableViewController
+
+@end
